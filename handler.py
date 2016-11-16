50c39ce17c45b6cb16c2abcc2aef6bcc96f7ca89
ldream <821173262@qq.com>
2016-11-16 18:00:53 +0800

    修改示例，DEMO

--- handler.py
+++ handler.py
@@ -1,4 +1,7 @@
-from FileMonitor import created, start
+import subprocess
+import sys
+
+from FileMonitor import created, start, deleted
 
 
 @created(r'py', once=True)
@@ -12,9 +15,17 @@ def pys(src_path):
 @start(r'.*py', once=True, )
 def sl(src_path):
     print("sl:", src_path)
+    command = ['python3', src_path]
+    print('Start process %s...' % ' '.join(command))
+    process = subprocess.Popen(command, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
 
-    @created(src_path, once=True, escape=True)
+    @deleted(src_path, once=True, escape=True, process=process)
     def fk(src_path):
+        print('Kill process [%s]...' % fk.__data__['process'].pid)
+        fk.__data__['process'].kill()
+        fk.__data__['process'].wait()
+        print('Process ended with code %s.' % fk.__data__['process'].returncode)
+        process = None
         print("fk:", src_path)
 
     sl.__app__.register_callback(fk)
